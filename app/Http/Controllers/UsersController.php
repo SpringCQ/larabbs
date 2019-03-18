8838dc5bbab11a665e208ec5aafef65e14dfdafe
Lio <lio@qq.com>
2019-03-19 01:05:17 +0800

    生成话题骨架 && 上传图片裁剪

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);//权限控制 身份验证
+    }
+
     //个人中心界面
     public function show(User $user)
     {
@@ -18,21 +23,24 @@ class UsersController extends Controller
     //修改个人信息界面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //更新个人信息
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $user->update($request->all());
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
 
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="http://www.baidu.com" target="_blank">Lio</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="float-right"><a href="mailto:276600085@qq.com">联系我们</a></p>
+        <p class="float-right"><a href="mailto:276600085@qq.com">联系我</a></p>
     </div>
 </footer>
