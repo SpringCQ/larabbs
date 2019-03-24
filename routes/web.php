cd3b4e723a5dd66366f5f6fbe5611426a36440b6
Lio <lio@qq.com>
2019-03-25 01:44:13 +0800

    回复列表 & 发布回复

--- routes/web.php
+++ routes/web.php
@@ -42,4 +42,6 @@ Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 
 //话题分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//话题回复
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
