9f3314dd8b82daf0b15d005a467174e05b57749e
Lio <lio@qq.com>
2019-03-26 21:21:49 +0800

    邮件通知 && 删除回复

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -59,4 +59,10 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
+    public function updateReplyCount()
+    {
+        $this->recount_count = $this->replies->count();
+        $this->save();
+    }
 }
